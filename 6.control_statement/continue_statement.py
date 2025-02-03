693af62831ce32f13f3bdcbc22783a7880b77a02
dinesku6 <dinesku6@cisco.com>
2025-02-03 20:07:45 +0530

    Day 5

---
+++ 6.control_statement/continue_statement.py
@@ -0,0 +1,13 @@
+"""
+
+continue statement
+To stop the loop
+
+"""
+
+i = 0
+while i <= 10:
+    i += 1
+    if i == 5:
+        continue
+    print(i)
---
+++ 7.For_loop/intro.py
@@ -0,0 +1,17 @@
+# 1  to 10
+
+for i in range(1,11):
+    print(i, end= " ")
+print()
+
+#0 to 10
+
+for i in range(0,11):
+    print(i,end=" ")
+print()
+
+# 2 4 6 8
+
+for i in range(2,11,2):
+
+    print(i,end=" ")
