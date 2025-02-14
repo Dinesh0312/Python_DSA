09902f1ad5055f790474ff2c4127f0b3ed613d72
dinesku6 <dinesku6@cisco.com>
2025-02-14 21:45:35 +0530

    List method

---
+++ 10.List/global_function.py
@@ -0,0 +1,10 @@
+my_list = [3,4,5,7,8]
+print(max(my_list))
+print(min(my_list))
+print(sum(my_list))
+print(len(my_list))
+
+
+my_list.extend([1,2])
+
+print(my_list)
---
+++ 10.List/indexind_traversal.py
@@ -0,0 +1,7 @@
+
+my_list = [5, 7, 33, 21, 56, 38, 76, 45, 432, 4, 543, 54, 654, 654, 654]
+
+n = len(my_list)
+print(n)
+for i in range(0, n):
+    print(my_list[i])
