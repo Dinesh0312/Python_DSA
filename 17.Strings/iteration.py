83c410dab6d4e703a6b885971e73052cf6408d85
dinesku6 <dinesku6@cisco.com>
2025-02-20 10:33:14 +0530

    sets and strings

---
+++ 17.Strings/iteration.py
@@ -0,0 +1,13 @@
+a = "anirudh khurana is a coder"
+
+# By Index
+
+n = len(a)
+
+for i in range(0, n):
+    print(a[i])
+
+# By Value
+
+for ch in a:
+    print(ch)
---
+++ 17.Strings/printing_reverse.py
@@ -0,0 +1,13 @@
+a = "anirudh khurana is a$#@$# coder"
+
+n = len(a)
+
+# for ch in a[::-1]:
+
+#     print(ch)
+
+
+
+for i in range(n - 1, -1, -1):
+
+    print(a[i])
